e770e87b78f120ddf422dbdc801d16ab4f059075
pranaysj <pranaysj3@gmail.com>
2022-11-16 12:35:39 +0530

    reupload fff files

---
+++ Script/AdsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize("4367065");
+    }
+
+    public void ShowBanner()
+    {
+        if (Advertisement.IsReady("Banner_Android"))
+        {
+            Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+            Advertisement.Banner.Show("Banner_Android");
+        }
+        else
+        {
+            StartCoroutine(ReadyShowBanner());
+        }
+    }
+
+    IEnumerator ReadyShowBanner()
+    {
+        yield return new WaitForSeconds(1.0f);
+        ShowBanner();
+    }
+}
---
+++ Script/UIScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    [SerializeField] GameObject gameOverSceneByMenuButton;
+    [SerializeField] GameObject insideeOptionMenu;
+    
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(RestartGameCall());
+    }
+
+    public IEnumerator RestartGameCall()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameOverSceneByMenuButton.SetActive(false);
+        SceneManager.LoadScene("Dot_Level_1");
+    }
+
+    public void QuitGame()
+    {
+        StartCoroutine(QuitGameCall());
+    }
+
+    public IEnumerator QuitGameCall()
+    {
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(0.5f);
+        Application.Quit();
+    }
+
+    public void MenuButton()
+    {
+        gameOverSceneByMenuButton.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void OptionButton()
+    {
+        insideeOptionMenu.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        insideeOptionMenu.SetActive(false);
+    }
+
+    public void ResumeButton()
+    {
+        gameOverSceneByMenuButton.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
